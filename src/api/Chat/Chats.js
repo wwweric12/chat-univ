9e7a9ec48f5c74f828850d86374deeb5a31a6dfc
unknown <pih9134@naver.com>
2023-11-17 03:21:58 +0900

    chore: 토큰 삭제

--- src/api/Chat/Chats.js
+++ src/api/Chat/Chats.js
@@ -6,7 +6,7 @@ export const getChats = async () => {
   try {
     const response = await axios.get("/api/chats", {
       headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhQGEuY29tIiwiaWF0IjoxNzAwMTQ3NTU0LCJleHAiOjE3MDAxNTExNTR9.9Q5P9nhryDLl5fgBlpcuvMT63HPL7oZfFbhG64aB_Cw`,
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
       },
     });
 
